1413a03034b4340ad2248bd33ffcc25ee6b15575
Emmanuel Obiora <68205094+obiora198@users.noreply.github.com>
2023-08-14 22:58:28 +0100

    add fcc test cdn and passed all tests

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script src='https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js' />
       </body>
     </Html>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,59 @@
 import { marked } from "marked";
-import { useState } from "react";
-import { markedHighlight } from "marked-highlight";
-import hljs from "highlight.js";
+import { useState } from "react"; 
 
-marked.use({
-    breaks: true,
-    gfm: true,
-});
+
 
 export default function Home() {
-  const [text,setText] = useState('');
+  const placeholder = 
+`# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`;
+
+  const [text,setText] = useState(placeholder);
+  const markdown = marked.parse(text, {breaks:true});
+  const renderer = new marked.Renderer();
 
   return (
     <div className='w-full flex flex-col items-center justify center gap-4 px-4 mt-6'>
@@ -19,7 +63,7 @@ export default function Home() {
             </div>
             <textarea
             name='text'
-            id='text'
+            id='editor'
             className='w-full h-[200px] p-2 focus:outline-0 text-sm'
             value={text}
             onChange={(e) => setText(e.target.value)}></textarea>
@@ -28,31 +72,12 @@ export default function Home() {
             <div className='w-full h-10 flex items-center justify-center bg-sky-300 border-b border-solid border-black'>
                 <h3 className='font-bold'>Preview</h3>
             </div> 
-            <Preview markdown={text} />
+            <div id="preview"
+            className='px-4 py-6'
+            dangerouslySetInnerHTML= {{ __html: marked(markdown, {renderer: renderer})}}
+            />
+            
         </div>
     </div>
   )
-}
-
-function Preview({ markdown }) {
-  const renderer = new marked.Renderer();
-  const marked = new Marked(
-    markedHighlight({
-      langPrefix: 'hljs language-',
-      highlight(code, lang) {
-        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-        return hljs.highlight(code, { language }).value;
-      }
-    })
-  );
-
-  return <div
-  
-    dangerouslySetInnerHTML={{
-        __html: marked(markdown, {renderer:renderer}),
-    }}
-    className='px-4 py-6'
-  >
-
-  </div>
 }
